c430f0f63d7d96fa5b601b667a4ddeeef87f2256
Laurant Marijnissen <laurant@gigafyde.dev>
2021-10-11 13:43:25 +0200

    initial header concept

---
+++ header.php
@@ -0,0 +1,9 @@
+<div class="menu">
+    <span class="item">
+        <img src="img/logo.png">
+    </span>
+    <span class="item">knop</span>
+    <span class="item">knop</span>
+    <span class="item">knop</span>
+    <span class="item">knop</span>
+</div>
