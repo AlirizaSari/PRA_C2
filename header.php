f559f42fc40bdff35a3afe73c396e483e5d6808d
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-14 10:41:11 +0200

    Fixed header

--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <header>
-    <div class="menu">
-        <div class="container">    
-        
-            <span class="item">
-                <a href="index.php"><img src="img/logo.png"></a>
-            </span>
-            <span class="item"><a id="header-link" href="">Songteksten</a></span>
-            <span class="item"><a id="header-link" href="">Informatie</a></span>
-            <span class="item"><a id="header-link" href=""></a></span>
-            <span class="item"><a id="header-link" href=""></a></span>
+    <div class="container">
+        <div class="menu">
+            <img src="img/logo.png" alt="CodeCamp logo">
+            <div class="splitPicture">
+                <a href="">Songteksten</a>
+                <a href="">Informatie</a>
+                
+            </div>
         </div>
     </div>
 </header>
