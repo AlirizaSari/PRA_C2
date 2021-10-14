4e8879e6ed8591e6d0eaf00225290f3a9c964bba
BigOlDumplin <jessedebruin1112@gmail.com>
2021-10-14 11:38:36 +0200

    Header design change + more links

--- header.php
+++ header.php
@@ -3,10 +3,17 @@
         <div class="menu">
             <a href="index.php"><img src="img/logo.png" alt="CodeCamp logo"></a>
             <div class="splitPicture">
+<<<<<<< Updated upstream
                 <a id="header-link" href="">Songteksten</a>
                 <a id="header-link" href="">Informatie</a>
                 <a id="header-link" href="">Dranken</a>
                 <a id="header-link" href="">Acteurs</a>
+=======
+                <a href="songtext.php">Songteksten</a>
+                <a href="">Informatie</a>
+                <a href="">Dranken</a>
+                <a href="">Acteurs</a>
+>>>>>>> Stashed changes
             </div>
         </div>
     </div>
