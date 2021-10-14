bd9fc2c4869f107841d14de3bea6ddc27b43dcde
BigOlDumplin <jessedebruin1112@gmail.com>
2021-10-14 11:38:56 +0200

    Added songtext.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/main.css">
         <link rel="shortcut icon" href="img/logo.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Mono">
         <title>Homepagina</title>
 </head>
 <body>
