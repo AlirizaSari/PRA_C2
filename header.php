c3f3d4f87566ff614dae0570938d23325d688329
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-14 11:44:44 +0200

    Fixed header (again)

--- header.php
+++ header.php
@@ -3,17 +3,11 @@
         <div class="menu">
             <a href="index.php"><img src="img/logo.png" alt="CodeCamp logo"></a>
             <div class="splitPicture">
-<<<<<<< Updated upstream
-                <a id="header-link" href="">Songteksten</a>
+                <a id="header-link" href="">Lyrics</a>
+                <a id="header-link" href="">Films</a>
                 <a id="header-link" href="">Informatie</a>
                 <a id="header-link" href="">Dranken</a>
                 <a id="header-link" href="">Acteurs</a>
-=======
-                <a href="songtext.php">Songteksten</a>
-                <a href="">Informatie</a>
-                <a href="">Dranken</a>
-                <a href="">Acteurs</a>
->>>>>>> Stashed changes
             </div>
         </div>
     </div>
