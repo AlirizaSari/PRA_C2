f88a71f41244b1a1e5a84af976e9c1fe6b9314ac
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-15 13:04:02 +0200

    added page Killer Queen

---
+++ Pages/Lyrics/killerQueen.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="shortcut icon" href="../../img/logo.ico">
+    <title>Homepagina</title>
+</head>
+<body>
+    <?php include "../../header.php"?>
+
+    <div class="content bohemBlue">
+        <div class="container">
+    
+            <h1>Killer Queen</h1>
+            <pre>
+                
+            [Verse 1]
+She keeps Moët et Chandon
+In her pretty cabinet
+"Let them eat cake," she says
+Just like Marie Antoinette
+A built-in remedy
+For Khrushchev and Kennedy (Ooh, ooh)
+At anytime an invitation
+You can't decline (Ooh, ooh)
+
+[Pre-Chorus]
+Caviar and cigarettes
+Well versed in etiquette
+Extraordinarily nice
+
+[Chorus]
+She's a Killer Queen
+Gunpowder, gelatine
+Dynamite with a laser beam
+Guaranteed to blow your mind
+(Pa-pa-pa-pa) Anytime
+Ooh
+Recommended at the price
+Insatiable an appetite
+Wanna try?
+[Verse 2]
+To avoid complications
+She never kept the same address
+In conversation
+She spoke just like a baroness
+Met a man from China
+Went down to Geisha Minah (Ooh, ooh)
+(Killer, killer, she's a killer Queen)
+Then again incidentally
+If you're that way inclined
+
+[Pre-Chorus]
+Perfume came naturally from Paris (Naturally)
+For cars, she couldn't care less
+Fastidious and precise
+
+[Chorus]
+She's a Killer Queen
+Gunpowder, gelatin
+Dynamite with a laser beam
+Guaranteed to blow your mind
+(Pa-pa-pa-pa)
+Anytime
+
+[Guitar Solo]
+[Bridge]
+Drop of a hat she's as willing as
+Playful as a pussy cat (Ooh)
+Then momentarily out of action (Ooh)
+Temporarily out of gas (Ta-taaa)
+To absolutely drive you wild, wild
+She's out to get you
+
+[Chorus]
+She's a Killer Queen
+Gunpowder, gelatine
+Dynamite with a laser beam (Pa-pa-pa-ra)
+Guaranteed to blow your mind
+Anytime
+Ooh
+Recommended at the price
+Insatiable an appetite
+Wanna try?
+You wanna try .
+
+
+            </pre>
+        </div>
+    </div>
+
+
+
+    <?php include "../../footer.php"?>
+</body>
+</html>
