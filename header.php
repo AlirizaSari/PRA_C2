5cf7edbd78cf5d2b7533a52a45aabe48a5b67e23
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-15 12:22:53 +0200

    Fixed logo in header

--- header.php
+++ header.php
@@ -1,12 +1,13 @@
+<?php require_once('backend/config.php');?>
 <header>
     <div class="container">
         <div class="menu">
-            <a href="index.php"><img src="img/logo.png" alt="CodeCamp logo"></a>
+            <a href="index.php"><img src="<?php echo $base_url; ?>/img/logo.png" alt="CodeCamp logo"></a>
             <div class="splitPicture">
                 <div class="dropdown">
                     <button class="dropbtn">Songteksten</button>
                     <div class="dropdown-content">
-                        <a href="songtext.php">Songtekst 1</a>
+                        <a href="Lyrics/bohemianRhapsody.php">Songtekst 1</a>
                         <a href="#">Songtekst 2</a>
                         <a href="#">Songtekst 3</a>
                     </div>
