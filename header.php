2adae4bef0ca0e7c8e1ad71845f78c1d2deae425
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-10-15 20:49:43 +0200

    frisdrank page fixed

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
                 <div class="dropdown">
                     <button class="dropbtn">Dranken</button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $base_url;?>/Pages/Dranken/Frisdrank.php">Frisdranken</a>
+                        <a href="<?php echo $base_url;?>/Pages/Dranken/frisdrank.php">Frisdranken</a>
                         <a href="<?php echo $base_url;?>/Pages/Dranken/Warme_Dranken.php">Warme dranken</a>
                     </div>
                 </div>
