e1867ae48ec9e8680fd1d24f57a2b2f3a3ed31c2
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-15 18:32:32 +0200

    grouped the saws

--- Pages/Informatie/gereedschap.php
+++ Pages/Informatie/gereedschap.php
@@ -21,11 +21,11 @@
             <br>
             <p>
                 <ul>
-                    <li><a href="gereedschappen/cirkelzagen.php">Cirkelzagen</a></li>
-                    <li><a href="gereedschappen/kettingzagen.php">Kettingzagen</a></li>
+                    <li><a href="gereedschappen/zagen/zagen.php">Zagen</a></li>
+                   
                     <li><a href="gereedschappen/hamers.php">Hamers</a></li>
                     <li><a href="gereedschappen/schroevendraaiers.php">Schroevendraaiers</a></li>
-                    <li><a href="gereedschappen/handzagen.php">Handzagen</a></li>
+                    
                 </ul>
             </p>
 
---
+++ Pages/Informatie/gereedschappen/zagen/zagen.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../../css/main.css">
+    <link rel="shortcut icon" href="../../../../img/logo.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Mono">
+    <title>Homepagina</title>
+</head>
+<body>
+    <?php include "../../../../header.php"?>
+
+    <div class="content padding">
+        <div class="container">
+    
+            <h2>Zagen</h2>
+            <br>
+            <p>Hier is de lijst met zagen:</p>
+            <br>
+            <p>
+                <ul>
+                    <li><a href="kettingzagen.php">Kettingzagen</a></li>
+                    <li><a href="handzagen.php">Handzagen</a></li>
+                    <li><a href="cirkelzagen.php">Cirkelzagen</a></li>
+                </ul>
+            </p>
+
+
+        </div>
+    </div>
+
+
+
+    <?php include "../../../../footer.php"?>
+</body>
+</html>
