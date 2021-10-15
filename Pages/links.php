8edcf68903193d4a90055797358220b91b91cec4
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2021-10-15 17:42:55 +0200

    Linkpagina

---
+++ Pages/links.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="shortcut icon" href="../img/logo.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Mono">
+    <title>Homepagina</title>
+</head>
+<body>
+    <?php include "../header.php"?>
+
+    <div class="content padding">
+        <div class="container">
+    
+            <h2>Links</h2>
+            <p><br></p>
+            <ul>
+                <li><a href="https://www.imdb.com/">IMDB</a></li>
+                <li><a href="https://www.videoland.com/nl/">VideoLand</a></li>
+                <li><a href="https://www.themovies.nl/">Movies.nl</a></li>
+            </ul>
+    </div>
+
+
+
+    <?php include "../footer.php"?>
+</body>
+</html>
