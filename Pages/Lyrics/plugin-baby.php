27a0f1f2e5256958027f3141c55c9a9a25cdf16b
Laurant Marijnissen <laurant@gigafyde.dev>
2021-10-18 14:13:05 +0200

    add files back

---
+++ Pages/Lyrics/plugin-baby.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../css/main.css">
+        <link rel="shortcut icon" href="../../img/logo.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Mono">
+        <title>Homepagina</title>
+    </head>
+    <body>
+        <?php include "../../header.php" ?>
+
+        <div class="content orange">
+            <div class="container">
+
+                <h1>Plug in Baby - Muse</h1>
+                <pre>
+
+I've exposed your lies
+Baby
+The underneath's no big surprise
+Now it's time for changing
+And cleansing everything to forget your love
+
+My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+Is tired of living
+
+Don't confuse
+Baby your gonna lose
+Your own game
+Change me
+Replace the envying
+To forget your love
+
+
+My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+Is tired of living
+
+And I've seen your loving
+Mine is gone
+
+And I've been in trouble
+wooaahhhh
+
+
+
+<br>
+<br>
+
+            </pre>
+            </div>
+        </div>
+
+
+        <?php include "../../footer.php" ?>
+    </body>
+</html>
