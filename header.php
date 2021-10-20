65e15dfaf5686421dbd160e903c40fabe93318b4
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-10-20 11:36:34 +0200

    Made some changes.
    - Added home button to the menu.
    - Renamed films to acteurs.
    - Added artistnames to the songs in the menu.

--- header.php
+++ header.php
@@ -4,26 +4,27 @@
         <div class="menu">
             <a href="<?php echo $base_url; ?>/index.php"><img src="<?php echo $base_url; ?>/img/logo.png" alt="CodeCamp logo"></a>
             <div class="splitPicture">
+                <a class="home-link" href="<?php echo $base_url; ?>">Home</a>
                 <div class="dropdown">
                     <button class="dropbtn">Lyrics</button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/bohemian-rhapsody.php">Bohemian Rhapsody</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/killer-queen.php">Killer Queen</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/madness.php">Madness</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/uprising-mybaby.php">Uprising MyBaby</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/uprising-muse.php">Uprising Muse</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/plugin-baby.php">Plug in Baby</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/transylvania.php">Transylvania</a>
-                        <a href="<?php echo $base_url; ?>/pages/lyrics/champions.php">We are the Champions</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/bohemian-rhapsody.php">Bohemian Rhapsody - Queen</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/killer-queen.php">Killer Queen - Queen</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/madness.php">Madness - Muse</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/uprising-mybaby.php">Uprising - MyBaby</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/uprising-muse.php">Uprising - Muse</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/plugin-baby.php">Plug in Baby - Muse</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/transylvania.php">Transylvania - Iron Maiden</a>
+                        <a href="<?php echo $base_url; ?>/pages/lyrics/champions.php">We are the Champions - Queen</a>
                     </div>
                 </div>
                 <div class="dropdown">
-                    <button class="dropbtn">Films</button>
+                    <button class="dropbtn">Acteurs</button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $base_url; ?>/pages/films/roger-moore.php">Roger Moore</a>
-                        <a href="<?php echo $base_url; ?>/pages/films/timothy-dalton.php">Timothy Dalton</a>
-                        <a href="<?php echo $base_url; ?>/pages/films/george-clooney.php">George Clooney</a>
-                        <a href="<?php echo $base_url; ?>/pages/films/filmpersonages.php">Filmpersonages</a>
+                        <a href="<?php echo $base_url; ?>/pages/acteurs/roger-moore.php">Roger Moore</a>
+                        <a href="<?php echo $base_url; ?>/pages/acteurs/timothy-dalton.php">Timothy Dalton</a>
+                        <a href="<?php echo $base_url; ?>/pages/acteurs/george-clooney.php">George Clooney</a>
+                        <a href="<?php echo $base_url; ?>/pages/acteurs/filmpersonages.php">Filmpersonages</a>
 
                     </div>
                 </div>
