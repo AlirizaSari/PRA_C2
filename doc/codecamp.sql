16340d3eaddc3fbf8f78b2fb4871ddcf620e46bf
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-11-08 12:02:01 +0100

    Changed sql

--- doc/codecamp.sql
+++ doc/codecamp.sql
@@ -1,34 +1,49 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server versie:                5.7.33 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Versie:              11.2.0.6213
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Gegenereerd op: 08 nov 2021 om 11:01
+-- Serverversie: 5.7.33
+-- PHP-versie: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Structuur van  tabel codecamp.boeken wordt geschreven
-CREATE TABLE IF NOT EXISTS `boeken` (
+--
+-- Database: `codecamp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `boeken`
+--
+
+CREATE TABLE `boeken` (
   `titel` varchar(50) NOT NULL,
   `auteur` varchar(50) NOT NULL,
   `jaartal` int(11) NOT NULL DEFAULT '0',
   `pagina` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumpen data van tabel codecamp.boeken: ~2 rows (ongeveer)
-/*!40000 ALTER TABLE `boeken` DISABLE KEYS */;
-INSERT INTO `boeken` (`titel`, `auteur`, `jaartal`, `pagina`) VALUES
-	('De dag van de doden', 'Nicci French', 2018, 365),
-	('Invictus Library - Je bent zoals je denkt', 'Michael Pilarczyk', 2021, 176),
-	('It Ends With Us', 'Colleen Hoover', 2016, 384);
-/*!40000 ALTER TABLE `boeken` ENABLE KEYS */;
+--
+-- Gegevens worden geëxporteerd voor tabel `boeken`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+INSERT INTO `boeken` (`titel`, `auteur`, `jaartal`, `pagina`) VALUES
+('De dag van de doden', 'Nicci French', 2018, 365),
+('Invictus Library - Je bent zoals je denkt', 'Michael Pilarczyk', 2021, 176),
+('It Ends With Us', 'Colleen Hoover', 2016, 384);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
