42f5149bc8b4abe2887e9ceaa4b85c61213341a3
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-11-08 09:22:32 +0100

    Warme dranken changed

--- pages/dranken/warme_dranken.php
+++ pages/dranken/warme_dranken.php
@@ -20,14 +20,56 @@
             <br>
             <p>Hier een lijst met warme dranken:</p>
             <br>
-            <p>
-            <ul>
-                <li><a href="warme_dranken/koffie.php">Koffie</a></li>
-                <li><a href="warme_dranken/latte.php">Latte</a></li>
-                <li><a href="warme_dranken/thee.php">Thee</a></li>
-            </ul>
-            </p>
 
+            <div class="latte-page">
+                <h2>Latte</h2>
+                <br>
+                <p>Een caffè latte of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde koffiedrank,
+                    gemaakt met espresso en warme melk. Vermoedelijk ligt de oorsprong in de Verenigde Staten, waar de
+                    drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te verzachten. In
+                    Italie wordt de term niet of nauwelijks gebruikt. Wie latte bestelt, krijgt melk.
+
+                    In veel Noord-Europese landen wordt het wel aangeduid als café au lait, die eveneens aan het eind
+                    van de 20e eeuw zou zijn ontstaan., in Frankrijk als grand crème en in Duitsland als Milchkaffee.
+
+                    Sommige barista's verstaan de kunst om de melk zodanig aan de koffie toe te voegen dat er ware
+                    kunstwerkjes ontstaan. Deze techniek wordt latte art genoemd.</p>
+
+            </div>
+
+            <div class="koffie-page">
+                <h2>Koffie</h2>
+                <br>
+                <p>Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en
+                    gebrande pitten van de koffieplant (Coffea spp.) die vanwege hun vorm koffiebonen worden genoemd.
+                    Koffie bevat het stimulerende middel cafeïne.[1][2] De meeste soorten in het geslacht Coffea komen
+                    van nature voor in tropisch Afrika en op de eilanden in de Indische Oceaan. Ze vinden hun oorsprong
+                    in Ethiopië, Jemen en Soedan.
+
+                    De koffieboon is een belangrijk exportproduct van landen rondom de evenaar, zoals Brazilië, Vietnam
+                    en Colombia. Koffieplanten worden vooral gecultiveerd op plantages in tropische landen, voor export
+                    naar landen met een gematigd klimaat. Samen met kennis van de drank verspreidde de plant zich vanaf
+                    de 15e eeuw uit tropisch Afrika naar Noord-Afrika, het Midden-Oosten en Europa. Sinds de 19e eeuw
+                    bevinden de belangrijkste producenten en exporteurs van koffiebonen zich in de tropische gebieden
+                    van Zuid- en Centraal-Amerika, Afrika en Azië. De meest geteelde soorten zijn Coffea arabica
+                    (arabica) en C. canephora (robusta).</p>
+                <br>
+                <p>Link naar de pagina over
+                <div class="isVerwijzing"><a href="<?php echo $base_url; ?>/pages/filmwereld/acteurs/george-clooney.php">George
+                        Clooney</a></div>
+                </p>
+            </div>
+
+            <div class="thee-page">
+                <h2>Thee</h2>
+                <br>
+                <p>Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant
+                    Camellia sinensis. Alle als thee verkochte producten die niet van deze plant afkomstig zijn
+                    (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.
+
+                    De naam thee komt waarschijnlijk uit het Chinese dialect Minnanyu.</p>
+
+            </div>
 
         </div>
     </div>
