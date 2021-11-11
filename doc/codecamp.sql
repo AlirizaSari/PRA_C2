36dfd6bbbd09193c01b30d0f4a0daf06b07809d6
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-11-11 10:42:46 +0100

    Update codecamp.sql

--- doc/codecamp.sql
+++ doc/codecamp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 10 nov 2021 om 20:12
+-- Gegenereerd op: 11 nov 2021 om 09:42
 -- Serverversie: 5.7.33
 -- PHP-versie: 8.0.12
 
@@ -33,18 +33,19 @@ CREATE TABLE `boeken` (
   `auteur` varchar(50) NOT NULL,
   `jaartal` int(11) NOT NULL DEFAULT '0',
   `pagina` int(11) NOT NULL DEFAULT '0',
-  `prijs` decimal(8,2) NOT NULL
+  `prijs` decimal(8,2) NOT NULL,
+  `beschrijving` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `boeken`
 --
 
-INSERT INTO `boeken` (`id`, `titel`, `auteur`, `jaartal`, `pagina`, `prijs`) VALUES
-(1, 'De dag van de doden', 'Nicci French', 2018, 365, '14.99'),
-(2, 'Invictus Library - Je bent zoals je denkt', 'Michael Pilarczyk', 2021, 176, '14.99'),
-(3, 'It Ends With Us', 'Colleen Hoover', 2016, 384, '9.99'),
-(4, 'De jongen, de mol, de vos en het paard', 'Charlie Mackesy, Arthur Japin', 2020, 128, '19.99');
+INSERT INTO `boeken` (`id`, `titel`, `auteur`, `jaartal`, `pagina`, `prijs`, `beschrijving`) VALUES
+(1, 'De dag van de doden', 'Nicci French', 2018, 365, '14.99', NULL),
+(2, 'Invictus Library - Je bent zoals je denkt', 'Michael Pilarczyk', 2021, 176, '14.99', NULL),
+(3, 'It Ends With Us', 'Colleen Hoover', 2016, 384, '9.99', NULL),
+(4, 'De jongen, de mol, de vos en het paard', 'Charlie Mackesy, Arthur Japin', 2020, 128, '19.99', NULL);
 
 --
 -- Indexen voor geëxporteerde tabellen
