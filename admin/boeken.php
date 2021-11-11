d50761f7cb47175b6ab89e71abd52b0fc511e09b
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-11-11 09:12:26 +0100

    Update boeken.php

--- admin/boeken.php
+++ admin/boeken.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/main.css">
-        <link rel="shortcut icon" href="../../img/logo.ico">
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="shortcut icon" href="../img/logo.ico">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+Mono">
         <script src="https://kit.fontawesome.com/1793fc5f13.js" crossorigin="anonymous"></script>
         <title>Homepagina</title>
